fc5b9369a4db19cfc192bf7651fe1e0751e63c1c
Dzmitry Nikalayeu <dimitra86@list.ru>
2022-08-25 22:36:57 +0300

    feat: add nds

---
+++ index.js
@@ -0,0 +1,12 @@
+let a=123.16;
+let Num=Math.trunc(a);
+let aKopeika=Math.round((a-Num)*100);
+let nds=a*20/120;
+function changeWords(a){
+  let words='';
+  return a;
+}
+console.log(a);
+console.log(nds);
+console.log(Num);
+console.log(aKopeika);
